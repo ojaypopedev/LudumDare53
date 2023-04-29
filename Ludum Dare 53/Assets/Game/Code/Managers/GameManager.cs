91ab5e970e709093117944e3dc31299471711605
Rojax98 <jackroper.smith@gmail.com>
2023-04-29 14:53:58 +0100

    Setup game actions

--- Ludum Dare 53/Assets/Game/Code/Managers/GameManager.cs
+++ Ludum Dare 53/Assets/Game/Code/Managers/GameManager.cs
@@ -6,8 +6,28 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    // Actions
     public static System.Action onReset;
+    public static System.Action onGameStarted;
+    public static System.Action<CompleteState> onGameFinished;
 
+    // Enums
+    public enum CompleteState
+    {
+        WIN,
+        FAIL
+    }
+    public enum GameState
+    {
+        INIT,
+        MENU,
+        PLAYING,
+        COMPLETE,
+    }
+
+    public static GameState gameState;
+
+    // UNITY
     private void Awake()
     {
         instance = this;
@@ -18,8 +38,31 @@ public class GameManager : MonoBehaviour
         Reset();
     }
 
+    // PUBLIC
     public static void Reset()
     {
+        if (gameState == GameState.INIT) return;
 
+        gameState = GameState.INIT;
+
+        onReset?.Invoke();
+    }
+
+    public static void StarGame()
+    {
+        if (gameState == GameState.PLAYING) return;
+
+        gameState = GameState.PLAYING;
+
+        onGameStarted?.Invoke();
+    }
+
+    public static void CompleteLevel(CompleteState completeState)
+    {
+        if (gameState == GameState.COMPLETE) return;
+
+        gameState = GameState.COMPLETE;
+
+        onGameFinished?.Invoke(completeState);
     }
 }
