0d0c765f667f2a650aa00173cfd15a5e1a1ef485
Rojax98 <jackroper.smith@gmail.com>
2023-05-01 20:10:38 +0100

    Update Icons & models

--- Ludum Dare 53/Assets/Game/Code/Food/FoodObject.cs
+++ Ludum Dare 53/Assets/Game/Code/Food/FoodObject.cs
@@ -79,7 +79,7 @@ namespace HotDogCannon.FoodPrep
         public void Merge(FoodObject fromItem)
         {
 
-            EffectsManager.CreateParticles(EffectsManager.MergeFoodParticles, transform.position);
+            if(Application.isPlaying) EffectsManager.CreateParticles(EffectsManager.MergeFoodParticles, transform.position);
             fromItem.GetBehaviour().OnMerge(fromItem, this);
             currentPotentialMerge = null;
             onMergedFoodGlobal?.Invoke(this);
