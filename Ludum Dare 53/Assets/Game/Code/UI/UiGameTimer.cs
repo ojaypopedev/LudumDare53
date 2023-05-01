ce09694415cbf909cde5c768c8109b18d0bd8103
Oliver <44278867+ojaypopedev@users.noreply.github.com>
2023-05-01 20:00:13 +0100

    Added Timer Change UI

--- Ludum Dare 53/Assets/Game/Code/UI/UiGameTimer.cs
+++ Ludum Dare 53/Assets/Game/Code/UI/UiGameTimer.cs
@@ -7,10 +7,37 @@ public class UiGameTimer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
 
+    public GameObject timeChangeUIPrefab;
+
     private void Awake()
     {
         LevelManager.onGameTimerChanged += Refresh;
         GameManager.onReset += OnReset;
+        CustomerManager.onCompletedFoodOrder += ShowTimeChangeUI;
+    }
+
+    public void ShowTimeChangeUI(bool sucess, FoodOrder _)
+    {
+
+        if(GameManager.gameMode == GameManager.GameMode.ENDLESS)
+        {
+            GameObject timeChangeInstance = Instantiate(timeChangeUIPrefab, timeChangeUIPrefab.transform.parent);
+            timeChangeInstance.SetActive(true);
+
+            if (sucess)
+            {
+                timeChangeInstance.GetComponentInChildren<TextMeshProUGUI>().text = "+" + LevelManager.instance.successRewardTime;
+                timeChangeInstance.GetComponentInChildren<TextMeshProUGUI>().color = Color.green;
+            }
+            else
+            {
+                timeChangeInstance.GetComponentInChildren<TextMeshProUGUI>().text = "-" + LevelManager.instance.failurePunishTime;
+                timeChangeInstance.GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+            }
+
+            Destroy(timeChangeInstance, 2);
+        }
+
     }
 
     void OnReset()
