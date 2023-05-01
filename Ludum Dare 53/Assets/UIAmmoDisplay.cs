22b094f9ad8b2a05e51ea959ab1cbdea054f8f2d
Oliver <44278867+ojaypopedev@users.noreply.github.com>
2023-05-01 12:52:57 +0100

    Ammo Display Finished

--- Ludum Dare 53/Assets/UIAmmoDisplay.cs
+++ Ludum Dare 53/Assets/UIAmmoDisplay.cs
@@ -1,6 +1,7 @@
 using HotDogCannon.Player;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +9,17 @@ public class UIAmmoDisplay : MonoBehaviour
 {
     public Gun gunToDisplay;
     public GameObject[] images;
+    [SerializeField] TextMeshProUGUI textMesh;
 
     // Update is called once per frame
     void Update()
     {
+        GetComponent<CanvasGroup>().alpha = gunToDisplay.currentAmmo != null ? 1 : 0;
         for (int i = 0; i < images.Length; i++)
         {
-            images[i].SetActive(gunToDisplay.currentRounds >= i);
+            images[i].SetActive(gunToDisplay.currentRounds > i);
         }
+
+        textMesh.text = gunToDisplay.currentRounds.ToString();
     }
 }
